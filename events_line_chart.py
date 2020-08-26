ace0e1ccd604810efeac152d09f5dd20182bdcfc
Farouk Turki <farouk.turki.eng@gmail.com>
2020-08-26 18:16:53 +0200

    events line chart commit

--- events_line_chart.py
+++ events_line_chart.py
@@ -16,4 +16,28 @@ years = list()
 for message_row in cur :
     if message_row[0] not in years:
         years.append(message_row[0])
-print(sorted(years))
+
+#write lists to event_line_chart.js file containing lists of each year and number of landslide events in that year
+
+#writing first row
+fhand = open('event_line_chart.js','w') 
+fhand.write("event_line_chart = [ ['Year'")
+years.sort()
+for country in countries:
+    fhand.write(",'"+country+"'")
+fhand.write("]")
+#writing the rest of rows
+for year in years:
+    fhand.write(",\n['"+str(year)+"'")
+    for country in countries:
+        cur.execute('SELECT count(id) FROM Events where country=? and year = ?',(country,year))
+        q=cur.fetchone()
+        fhand.write(","+str(q[0]))
+    fhand.write("]")
+
+fhand.write("\n];\n")
+fhand.close()
+
+print("Output written to event_line_chart.js")
+print("Open event_line_chart.htm to visualize the data")
+ 
